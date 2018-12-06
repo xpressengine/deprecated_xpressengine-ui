31d059069441e71dc12a54f7be5d14a085492de1
bnu <bnufactory@gmail.com>
2018-12-06 17:04:04 +0900

    assets 빌드 환경 구성

---
+++ gulpfile.babel.js
@@ -0,0 +1,18 @@
+import gulp from 'gulp'
+import plugins from 'gulp-load-plugins'
+
+const $ = plugins()
+
+gulp.task('default', ['sass'])
+
+gulp.task('sass', () => {
+  return gulp.src('./src/scss/*.scss')
+    .pipe($.sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('watch', function () {
+  gulp.watch('./src/scss/*.scss', ['sass'])
+})
